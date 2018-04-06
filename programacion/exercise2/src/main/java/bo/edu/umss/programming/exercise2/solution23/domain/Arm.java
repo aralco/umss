fc6321b64f945ddbdd35a760e0ef73b172a71444
Ariel Alcocer <ariel.alcocer@gmail.com>
2018-04-06 13:15:31 -0400

    Including a new solution for problem, to be implemented by Alex.

---
+++ programacion/exercise2/src/main/java/bo/edu/umss/programming/exercise2/solution23/domain/Arm.java
@@ -0,0 +1,107 @@
+package bo.edu.umss.programming.exercise2.solution23.domain;
+
+public class Arm implements Asset {
+    private String id;
+    private String name;
+    private String description;
+    private Tag tag;
+    private String serialNumber;
+    private String caliber;
+    private String weight;
+    private String manufacturer;
+
+    public Arm(String id, String name, String description, String serialNumber, String caliber, String weight, String manufacturer) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.serialNumber = serialNumber;
+        this.caliber = caliber;
+        this.weight = weight;
+        this.manufacturer = manufacturer;
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public Tag getTag() {
+        return tag;
+    }
+
+    @Override
+    public void setTag(Tag tag) {
+        this.tag = tag;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getCaliber() {
+        return caliber;
+    }
+
+    public void setCaliber(String caliber) {
+        this.caliber = caliber;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    @Override
+    public String toString() {
+        return "Arm{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", tag=" + tag +
+                ", serialNumber='" + serialNumber + '\'' +
+                ", caliber='" + caliber + '\'' +
+                ", weight='" + weight + '\'' +
+                ", manufacturer='" + manufacturer + '\'' +
+                '}';
+    }
+}
---
+++ programacion/exercise2/src/main/java/bo/edu/umss/programming/exercise2/solution23/domain/Tag.java
@@ -0,0 +1,5 @@
+package bo.edu.umss.programming.exercise2.solution23.domain;
+
+public interface Tag {
+    String type();
+}
