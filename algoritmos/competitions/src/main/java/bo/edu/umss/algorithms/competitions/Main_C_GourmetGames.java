20d977287e42aa5cce72b902348e0dacfc439ab4
Ariel Alcocer <ariel.alcocer@gmail.com>
2018-10-01 15:10:11 -0400

    Including solution for Gourment Games.

---
+++ algoritmos/competitions/src/main/java/bo/edu/umss/algorithms/competitions/Main_C_GourmetGames.java
@@ -0,0 +1,26 @@
+package bo.edu.umss.algorithms.competitions;
+
+import java.util.Scanner;
+
+public class Main_C_GourmetGames {
+    public static void main(String[] args)  {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        for(int i=0;i<t;i++)    {
+            getNumberOfShowsRequired(scanner.nextInt(), scanner.nextInt());
+        }
+    }
+
+    static void getNumberOfShowsRequired(int n, int m)  {
+        int showsNumber = 0;
+        while(n-m+1>=m)  {
+            showsNumber++;
+            n=n-m+1;
+        }
+        if(n==m)    {
+            System.out.println(++showsNumber);
+        } else  {
+            System.out.println("cannot do this");
+        }
+    }
+}
